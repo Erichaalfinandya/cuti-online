4922f8c2e1936a3aa544e3660cee1d38020aad00
marsaam <marsaamalia03@gmail.com>
2025-12-03 09:50:45 +0700

    panitera 123, sekretaris 12 done

--- app/Helpers/functions.php
+++ app/Helpers/functions.php
@@ -7,7 +7,7 @@ if (!function_exists('status_required_for')) {
 
         return match($golongan) {
             'kepegawaian' => 1,
-            'panmud_1', 'panmud_2', 'panmud_3' => 2,
+            'panmud_1', 'panmud_2', 'panmud_3', 'kasubbag_1', 'kasubbag_2', 'kasubbag_3' => 2,
             'panitera', 'sekretaris' => 3,
             'ketua' => 4,
             default => 0,
